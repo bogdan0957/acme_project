71764a5561296bb4e8075b57aba38aa21196e032
Bogda <bogdan0957@yandex.ru>
2023-11-10 21:54:21 +0400

    update

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path('birthday/', include('birthday.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
